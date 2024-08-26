afa9602851b94da0723a6999bddcbe0157b90712
VladyslavKurdijashko <159957674+VladyslavKurdijashko@users.noreply.github.com>
2024-08-26 20:43:33 +0300

    hw-3

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -4,55 +4,60 @@ import { nanoid } from "nanoid";
 import styles from "./ContactForm.module.css";
 
 const ContactForm = ({ onSubmit }) => {
-    const validationSchema = Yup.object({
-      name: Yup.string()
-        .min(3, "Min 3 characters")
-        .max(50, "Max 50 characters")
-        .required("Required"),
-      number: Yup.string()
-        .min(3, "Min 3 characters")
-        .max(50, "Max 50 characters")
-        .required("Required"),
-    });
-  
-    return (
-      <Formik
-        initialValues={{ name: "", number: "" }}
-        validationSchema={validationSchema}
-        onSubmit={(values, { resetForm }) => {
-          onSubmit({ id: nanoid(), ...values });
-          resetForm();
-        }}
-      >
-        {({ isSubmitting }) => (
-          <Form className={styles.form}>
-            <label htmlFor="name" className={styles.label}>
-              Name
-            </label>
-            <Field name="name" type="text" className={styles.input} />
-            <ErrorMessage name="name" component="div" className={styles.error} />
-  
-            <label htmlFor="number" className={styles.label}>
-              Number
-            </label>
-            <Field name="number" type="text" className={styles.input} />
-            <ErrorMessage
-              name="number"
-              component="div"
-              className={styles.error}
-            />
-  
-            <button
-              type="submit"
-              disabled={isSubmitting}
-              className={styles.button}
-            >
-              Add Contact
-            </button>
-          </Form>
-        )}
-      </Formik>
-    );
-  };
-  
-  export default ContactForm;
+  const validationSchema = Yup.object({
+    name: Yup.string()
+      .min(3, "Min 3 characters")
+      .max(50, "Max 50 characters")
+      .required("Required"),
+    number: Yup.string()
+      .min(3, "Min 3 characters")
+      .max(50, "Max 50 characters")
+      .required("Required"),
+  });
+
+  return (
+    <Formik
+      initialValues={{ name: "", number: "" }}
+      validationSchema={validationSchema}
+      onSubmit={(values, { resetForm }) => {
+        onSubmit({ id: nanoid(), ...values });
+        resetForm();
+      }}
+    >
+      {({ isSubmitting }) => (
+        <Form className={styles.form}>
+          <label htmlFor="name" className={styles.label}>
+            Name
+          </label>
+          <Field id="name" name="name" type="text" className={styles.input} />
+          <ErrorMessage name="name" component="div" className={styles.error} />
+
+          <label htmlFor="number" className={styles.label}>
+            Number
+          </label>
+          <Field
+            id="number"
+            name="number"
+            type="text"
+            className={styles.input}
+          />
+          <ErrorMessage
+            name="number"
+            component="div"
+            className={styles.error}
+          />
+
+          <button
+            type="submit"
+            disabled={isSubmitting}
+            className={styles.button}
+          >
+            Add Contact
+          </button>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+
+export default ContactForm;
